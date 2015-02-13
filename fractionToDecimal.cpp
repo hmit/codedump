67ff7f74737fb36f1885f784559fda95fc6593b7
Harshit Mittal <h.mittal83@gmail.com>
2015-02-13 01:56:35 -0800

    checkpoint

--- fractionToDecimal.cpp
+++ fractionToDecimal.cpp
@@ -13,43 +13,52 @@
 using namespace std;
 
 class Solution {
-  typedef vector< int > NList;
+  typedef long long int NType;
+  typedef vector< NType > NList;
   typedef NList::iterator NListIt;
 public:
-  int numTens( int n ) {
-    int r = 1;
-    while( n > 1 ) {
-      n /= 10;
-      r *= 10;
-    }
-    return r;
-  }
-
-  string fractionToDecimal( int num, int den ) {
+  string fractionToDecimal( NType num, NType den ) {
     if ( den == 0 )
       return "INF";
     stringstream ss;
+    if ( num * den < 0 )
+      ss << "-";
+    num = abs(num);
+    den = abs(den);
     ss << num / den;
-    map< int, bool > hashMap;
-    int remain = num % den;
+    cout << ss.str() << endl;
+    map< NType, bool > hashMap;
+    NType remain = abs( num % den );
     if ( remain == 0 )
       return ss.str();
     NList digits;
     while ( remain != 0 and !hashMap.count( remain ) ) {
+      remain *= 10;
       hashMap[ remain ] = true;
-      digits.push_back( remain );
-      while ( remain < den ) remain *= 10;
+      digits.push_back( -1 * remain );
+      cout << "remain " << remain << endl;
+
+      while ( remain < den ) {
+	digits.push_back( 0 );
+	remain *= 10;
+      }
       digits.push_back( remain / den );
-      remain = remain % den;
+      remain = ( remain % den );
     }
+    cout << "here" << endl;
     ss << ".";
     bool close = false;
     for (  NListIt it = digits.begin(); it != digits.end(); it++ ) {
-      if ( remain != 0 and *it == remain ) {
+      cout << "here" << *it << endl;
+      if ( remain != 0 and *it == -1 * remain ) {
 	ss << "(";
 	close = true;
       }
       it++;
+      while ( *it == 0 ) {
+	ss << *it;
+	it++;
+      }
       ss << *it;
     }
     if (close)
@@ -58,12 +67,16 @@ public:
   }
 };
 
+
 int main() {
-  int num, den;
+  long long int num, den;
   Solution sol;
+  cout << -1 / -2147483648 << " " << -1 % -2147483648 << endl;
+  ///*
   while( cin >> num ) {
     cin >> den;
     cout << sol.fractionToDecimal( num, den ) << endl;
   }
+  // */
   return 1;
 }
