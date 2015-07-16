6c2412747f37fa080c4c534d4b41082863c75ade
Harshit Mittal <h.mittal83@gmail.com>
2015-07-16 14:21:36 -0700

    indentation fix

--- Bowling.java
+++ Bowling.java
@@ -8,13 +8,13 @@ import java.nio.*;
 public class Bowling
 {
   private static boolean Debug = false;
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
+  public static void main (String[] args) throws java.lang.Exception
+  {
+    // your code goes here
     testBowlingScores();
     String mySheet = "2,3,x,,5,/,4,5,x,,x,,6,/,7,2,8,1,x,x,x";
-		System.out.println("totScore: " + getBowlingScore(mySheet));
-	}
+    System.out.println("totScore: " + getBowlingScore(mySheet));
+  }
 
   public static void testBowlingScores() {
     String mySheet = "2,3,x,,5,/,4,5,x,,x,,6,/,7,2,8,1,x,x,x";
@@ -26,40 +26,40 @@ public class Bowling
 
   public static int getBowlingScore(String scoreSheet) {
     String[] scores = new String(scoreSheet).split(",");
-		int totScore = 0, prev = 0, bonus = 0;
+    int totScore = 0, prev = 0, bonus = 0;
     int[] bonusArr = new int[scores.length];
     int counter = 0;
-		for (int i = 0; i < scores.length; i++) {
+    for (int i = 0; i < scores.length; i++) {
       if (scores[i].length() == 0) {
         continue;
       }
       bonusArr[counter] = 0;
-			char scChr = scores[i].trim().charAt(0);
-			if (scChr == '\0') {
-				continue;
-			}
-			int score = 0, nb = 0;
-			if (scChr >= '1' && scChr <= '9') {
-				score = scChr - '0';
-				prev = score;
-			}
-			if (scChr == '/') {
-				score = 10 - prev;
+      char scChr = scores[i].trim().charAt(0);
+      if (scChr == '\0') {
+        continue;
+      }
+      int score = 0, nb = 0;
+      if (scChr >= '1' && scChr <= '9') {
+        score = scChr - '0';
+        prev = score;
+      }
+      if (scChr == '/') {
+        score = 10 - prev;
         bonusArr[counter] = 1;
-			}
-			if (scChr == 'x') {
-				score = 10;
+      }
+      if (scChr == 'x') {
+        score = 10;
         bonusArr[counter] = 2;
-			}
+      }
 
       boolean p1Bonus = (counter - 1 >= 0 && i < 19 && bonusArr[counter - 1] > 0);
       boolean p2Bonus = (counter - 2 >= 0 && i < 20 && bonusArr[counter - 2] > 1);
-			totScore += score + ((p1Bonus) ? score : 0) + ((p2Bonus) ? score : 0);
+      totScore += score + ((p1Bonus) ? score : 0) + ((p2Bonus) ? score : 0);
       if (Debug) {
         System.out.println("char " + i + " " + scores[i] + " " + score + " " + totScore + " " + bonusArr[counter]);
       }
       counter++;
-		}
+    }
     return totScore;
   }
 }
